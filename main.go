6e19b6f03d38a8678fa22e0357f170e81c7c9127
tanickell <tanickell22@gmail.com>
2026-05-13 10:29:53 -0600

    Getting user input.

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ func main() {
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
-	// remainingTickets = -1 // won't work bc remainingTickets is uint type (see above)
-
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
 	fmt.Printf("Welcome to the %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets, with %v still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
@@ -18,8 +14,12 @@ func main() {
 	var userName string
 	var userTickets int
 	// ask the user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&userName)
 
-	userName = "Tom"
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets)
+
 	userTickets = 2
 	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets) // fmt.Println(userName)
 }
