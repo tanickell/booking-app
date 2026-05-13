1afb001da252246788199fd2f9af480abc95b2df
tanickell <tanickell22@gmail.com>
2026-05-13 10:04:06 -0600

    Contrasting vars and consts.

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
 	fmt.Println("Welcome to the", conferenceName, "booking application!")
+	// fmt.Println()
+	fmt.Println("We have a total of", conferenceTickets, "tickets, with", remainingTickets, "still available.")
 	fmt.Println("Get your tickets here to attend.")
 }
